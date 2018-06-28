1a654ce41744bdfbee11989cf0b389cd0211a45b
patrickatcw <pj8392@yahoo.com>
2018-06-28 13:29:29 -0400

    working through this example for jpa hibernate

---
+++ src/main/java/com/JavaJPAHibernateExample/JavaJPAHibernateExample/JavaJpaHibernateExampleApplication.java
@@ -0,0 +1,12 @@
+package com.JavaJPAHibernateExample.JavaJPAHibernateExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJpaHibernateExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaJpaHibernateExampleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/JavaJPAHibernateExample/JavaJPAHibernateExample/JavaJpaHibernateExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.JavaJPAHibernateExample.JavaJPAHibernateExample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavaJpaHibernateExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
